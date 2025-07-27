a625e9fadbdf372e400e09c8054497eab40f1666
GMenakshi <122555177+GMenakshi@users.noreply.github.com>
2025-07-27 13:34:51 +0530

    Update React components with new categories and logo

---
+++ src/app/download/page.jsx
@@ -0,0 +1,25 @@
+import DashboardDownload from "@/components/DashboardDownload";
+import Preloader from "@/helper/Preloader";
+import MasterLayout from "@/layout/MasterLayout";
+
+export const metadata = {
+  title: "Digital Market Place NEXT Js Template",
+  description:
+    "Digital Era – Digital Products Marketplace NEXT JS Template – A versatile and meticulously designed set of templates crafted to elevate your Digital Products Marketplace content and experiences.",
+};
+
+const page = () => {
+  return (
+    <>
+      <MasterLayout>
+        {/* Preloader */}
+        <Preloader />
+
+        {/* DashboardDownload */}
+        <DashboardDownload />
+      </MasterLayout>
+    </>
+  );
+};
+
+export default page;
---
+++ src/app/review/page.jsx
@@ -0,0 +1,25 @@
+import DashboardReview from "@/components/DashboardReview";
+import Preloader from "@/helper/Preloader";
+import MasterLayout from "@/layout/MasterLayout";
+
+export const metadata = {
+  title: "Digital Market Place NEXT Js Template",
+  description:
+    "Digital Era – Digital Products Marketplace NEXT JS Template – A versatile and meticulously designed set of templates crafted to elevate your Digital Products Marketplace content and experiences.",
+};
+
+const page = () => {
+  return (
+    <>
+      <MasterLayout>
+        {/* Preloader */}
+        <Preloader />
+
+        {/* DashboardReview */}
+        <DashboardReview />
+      </MasterLayout>
+    </>
+  );
+};
+
+export default page;
